d8a920ae2374ac29688180f4678dd95292a7d6b4
Nick Qvelidze <macbookpro@Qvelos-MacBook-Pro.local>
2025-08-26 21:22:55 +0400

    test

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -40,7 +40,7 @@ const AuthPage = () => {
                 </h1> */}
                 <a
                   href="/"
-                  className="flex items-center justify-content: center; space-x-2"
+                  className="flex items-center justify-center space-x-2"
                 >
                   <img
                     src="/lovable-uploads/a27089ec-2666-4c11-a0e0-0d8ea54e1d39.png"
