86b3ca8583c0a23f8187b3acd81ca5842cfc7f09
Nick Qvelidze <macbookpro@Qvelos-MacBook-Pro.local>
2025-08-26 22:14:09 +0400

    test

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -34,7 +34,7 @@ const AuthPage = () => {
         <div className="w-full max-w-md">
           {!selectedRole ? (
             <div className="space-y-6">
-              <div className="text-center mb-8">
+              <div className="text-center mb-8 space-x-3">
                 {/* <h1 className="text-3xl font-semibold text-foreground mb-2">
                   Welcome to Breneo
                 </h1> */}
@@ -62,7 +62,7 @@ const AuthPage = () => {
                     <div className="flex items-start space-x-4">
                       <div className="flex-shrink-0">
                         <div className="w-12 h-12 bg-primary/10 rounded-lg flex items-center justify-center">
-                          <GraduationCap className="h-6 w-6 text-blue-500" />
+                          <GraduationCap className="h-6 w-6 text-breneo-blue" />
                         </div>
                       </div>
                       <div className="flex-1">
@@ -89,7 +89,7 @@ const AuthPage = () => {
                     <div className="flex items-start space-x-4">
                       <div className="flex-shrink-0">
                         <div className="w-12 h-12 bg-primary/10 rounded-lg flex items-center justify-center">
-                          <Building2 className="h-6 w-6 text-blue-500" />
+                          <Building2 className="h-6 w-6 text-breneo-blue" />
                         </div>
                       </div>
                       <div className="flex-1">
