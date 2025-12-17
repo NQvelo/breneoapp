41a7f3b3c0318a86671da45fd99e182791921cce
Nick Qvelidze <macbookpro@Qvelos-MacBook-Pro.local>
2025-12-17 02:18:34 +0100

    posthog implacement in the platform

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "next-themes";
+import { PostHogProvider } from "posthog-js/react";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="light"
-      enableSystem={true}
-      disableTransitionOnChange
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        defaults: "2025-05-24",
+        capture_exceptions: true,
+        debug: import.meta.env.MODE === "development",
+      }}
     >
-      <div suppressHydrationWarning>
-        <App />
-      </div>
-    </ThemeProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem={true}
+        disableTransitionOnChange
+      >
+        <div suppressHydrationWarning>
+          <App />
+        </div>
+      </ThemeProvider>
+    </PostHogProvider>
   </React.StrictMode>
 );
