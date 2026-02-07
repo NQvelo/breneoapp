90a1e17b9a1ef0cada03d0b32e33eb77817f784b
Nick Qvelidze <macbookpro@Qvelos-MacBook-Pro.local>
2026-02-07 21:09:24 +0100

    updated favicon

--- vite.config.ts
+++ vite.config.ts
@@ -55,13 +55,13 @@ export default defineConfig(({ mode }) => ({
         start_url: "/",
         icons: [
           {
-            src: "/lovable-uploads/d42d6d67-a5c8-468e-a458-51e9b24eeec0.png",
+            src: "/lovable-uploads/breneo-favicon.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any maskable",
           },
           {
-            src: "/lovable-uploads/d42d6d67-a5c8-468e-a458-51e9b24eeec0.png",
+            src: "/lovable-uploads/breneo-favicon.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
@@ -76,7 +76,7 @@ export default defineConfig(({ mode }) => ({
             url: "/dashboard",
             icons: [
               {
-                src: "/lovable-uploads/d42d6d67-a5c8-468e-a458-51e9b24eeec0.png",
+                src: "/lovable-uploads/breneo-favicon.png",
                 sizes: "96x96",
               },
             ],
