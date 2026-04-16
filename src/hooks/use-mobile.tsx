fe522b3048470c7db636342bd324ee4b0da226b3
Nick Qvelidze <macbookpro@Qvelos-MacBook-Pro.local>
2026-04-16 04:19:48 +0200

    fix

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -4,7 +4,7 @@ const MOBILE_BREAKPOINT = 768; // Corresponds to Tailwind's `md` breakpoint
 
 export function useMobile(): boolean {
   const [isMobile, setIsMobile] = useState(
-    window.innerWidth < MOBILE_BREAKPOINT
+    window.innerWidth < MOBILE_BREAKPOINT,
   );
 
   useEffect(() => {
